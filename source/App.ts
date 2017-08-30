d65d29dccc3093d295833187cb7b88c83c3eb818
Daniel Miller <daniel@wants.email>
2017-08-29 22:59:01 -0500

    Add test for mostRecentlyPressedKey

--- source/App.ts
+++ source/App.ts
@@ -1,12 +1,15 @@
-import { pressedKeyCodesFromOldestToNewest, isKeyPressed } from './kepo'
+import { pressedKeyCodesFromOldestToNewest, isKeyPressed, mostRecentlyPressedKey } from './kepo'
 
 document.addEventListener('DOMContentLoaded', () => {
     const pressedKeysContainer = document.getElementById('pressed-keys')!
     const isSpacebarPressedContainer = document.getElementById('is-spacebar-pressed')!
+    const arrowRecencyContainer = document.getElementById('arrow-recency')!
 
     function frame() {
         pressedKeysContainer.innerHTML = pressedKeyCodesFromOldestToNewest.join(', ')
         isSpacebarPressedContainer.innerHTML = isKeyPressed(32).toString()
+        arrowRecencyContainer.innerHTML = (mostRecentlyPressedKey(37, 38, 39, 40) || 'None').toString()
+
         window.requestAnimationFrame(frame)
     }
 
