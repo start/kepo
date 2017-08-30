fbe30724eccbfcc5f5264237debd9e886074a83e
Daniel Miller <daniel@wants.email>
2017-08-29 22:50:17 -0500

    Add test for isKeyPressed

--- source/App.ts
+++ source/App.ts
@@ -1,11 +1,12 @@
-import { pressedKeyCodesFromOldestToNewest } from './kepo'
+import { pressedKeyCodesFromOldestToNewest, isKeyPressed } from './kepo'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const outputContainer = document.getElementById('pressed-keys')!
+    const pressedKeysContainer = document.getElementById('pressed-keys')!
+    const isSpacebarPressedContainer = document.getElementById('is-spacebar-pressed')!
 
     function frame() {
-        outputContainer.innerHTML = pressedKeyCodesFromOldestToNewest.join(', ')
-        
+        pressedKeysContainer.innerHTML = pressedKeyCodesFromOldestToNewest.join(', ')
+        isSpacebarPressedContainer.innerHTML = isKeyPressed(32).toString()
         window.requestAnimationFrame(frame)
     }
 
